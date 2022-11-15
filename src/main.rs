d435f0a36bf1d2994496ae7ec2b0cc0eeed7f601
Matthew S. Shoop <ShoopM294@gmail.com>
2022-11-15 17:18:45 -0500

    Finished Draft

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use mazegen::GameState;
+//use mazegen::GameState;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Grid, Label};
+use gtk::{gio, Application, ApplicationWindow, Button, Grid, Label};
+use gtk::glib::object::Cast;
 use std::{fs, io, thread};
 use std::time::Duration;
 
@@ -12,19 +13,33 @@ const APP_ID: &str = "org.gtk-rs.HyperspaceNav";
 pub mod mazegen;
 
 fn main() {
-    println!("Hello, world!");
-    if mazegen::make_maze(1) {
-        println!("Maze Made");
-    } else {
-        println!("Something went wrong");
+    //println!("Hello, world!");
+    let file = fs::OpenOptions::new().write(true).create_new(true).open("data");
+    match file {
+        Ok(_) => {if mazegen::make_maze(1) {
+                println!("Maze Made");
+            } else {
+                println!("Something went wrong");
+            }},
+        Err(error) => match error.kind() {
+            io::ErrorKind::AlreadyExists => {},
+            _ => panic!("Problem with lock file error {:?}", error),
+        },
     }
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
+    
+    app.set_accels_for_action("win.restart", &["r"]);
+    app.set_accels_for_action("win.forward", &["w", "Up"]);
+    app.set_accels_for_action("win.turn_left", &["a", "Left"]);
+    app.set_accels_for_action("win.turn_right", &["d", "Right"]);
+    app.set_accels_for_action("win.turn_back", &["s", "Down"]);
+
     app.run();
-    println!("End");
+    //println!("End");
 }
 
-fn forward_but_click(forward_but: Button) {
+fn forward_but_click(my_button: &Button) {
     //Lock File Loop
     let mut counter = 0;
     while counter < 10 {
@@ -43,16 +58,77 @@ fn forward_but_click(forward_but: Button) {
         return;
     }
     if mazegen::move_cell() {
-        // Update labels
-        println!("Moved");
+        let first_grid = my_button.parent().unwrap()
+                                    .parent().unwrap()
+                                    .parent().unwrap();
+        let mut child_widget = first_grid.first_child().unwrap();
+        let state_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+        update_state_label(&state_label);
+        let label_grid = first_grid.last_child().unwrap();
+        child_widget = label_grid.first_child().unwrap()
+                                    .next_sibling().unwrap();
+        let mut dimension_counter = 0;
+        while dimension_counter < 4 {
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((true, dimension_counter), &child_label);
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((false, dimension_counter), &child_label);
+            dimension_counter += 1;
+        }
+        //println!("Moved");
     } else {
-        println!("Cannot Move Forward");
+        //println!("Cannot Move Forward");
     }
     fs::remove_file("data.lock").unwrap();
 }
 
-fn update_label(direction: (bool, u32), text: Label) {
-    let mut result = String::from(".  ");
+fn restart_game(my_button: &Button) {
+    let mut counter = 0;
+    while counter < 10 {
+        counter += 1;
+        let file = fs::OpenOptions::new().write(true).create_new(true).open("data.lock");
+        match file {
+            Ok(_) => break,
+            Err(error) => match error.kind() {
+                io::ErrorKind::AlreadyExists => println!("Game Locked"),
+                _ => panic!("Problem with lock file error {:?}", error),
+            },
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+    if counter > 9 {
+        return;
+    }
+    if mazegen::make_maze(1) {
+        let first_grid = my_button.parent().unwrap()
+                                    .parent().unwrap()
+                                    .parent().unwrap();
+        let mut child_widget = first_grid.first_child().unwrap();
+        let state_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+        update_state_label(&state_label);
+        let label_grid = first_grid.last_child().unwrap();
+        child_widget = label_grid.first_child().unwrap()
+                                    .next_sibling().unwrap();
+        let mut dimension_counter = 0;
+        while dimension_counter < 4 {
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((true, dimension_counter), &child_label);
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((false, dimension_counter), &child_label);
+            dimension_counter += 1;
+        }
+    } else {
+        println!("Something went wrong");
+    }
+    fs::remove_file("data.lock").unwrap();
+}
+
+fn update_label(direction: (bool, u32), text: &Label) {
+    let mut result = String::from(".");
     result += match direction.1 {
         0 => "x ",
         1 => "y ",
@@ -64,15 +140,207 @@ fn update_label(direction: (bool, u32), text: Label) {
         }
     };
     if direction.0 {
-        result += "+  .\n";
+        result += "+.\n__\n";
     } else {
-        result += "-  .\n";
+        result += "-.\n__\n";
     }
-    // Label Update
+    let mut notes = mazegen::hall_dist(direction);
+    while notes.len() > 0 {
+        result += &("|".to_owned() + &notes.pop().unwrap() + "|\n");
+    }
+    text.set_label(&result);
+    /*if distance >= 0 {
+        for _ in 0..distance {
+            result += "|  |\n";
+        }
+        text.set_label(&result);
+    }*/
 }
 
-fn left_but_click () {
-    //Testing Button Edit
+fn update_state_label(text: &Label) {
+    let state: (u32, u32, (bool, u32), String) = mazegen::get_loc();
+    let mut position = state.0;
+    if position == (1 << 12) - 1 {
+        text.set_label("You Win!");
+        return;
+    }
+    let mut result = String::from("Position: (");
+    let direction = state.2;
+    result += &(position % 8).to_string();
+    position -= position % 8;
+    position = position >> 3;
+    let mut counter = 1;
+    while counter < state.1 {
+        result += ", ";
+        result += &(position % 8).to_string();
+        position -= position % 8;
+        position = position >> 3;
+        counter += 1;
+    }
+    result += ") Facing: ";
+    result += match direction.1 {
+        0 => "x ",
+        1 => "y ",
+        2 => "z ",
+        3 => "a ",
+        _ => {
+            println!("Something went wrong: {:?}", direction);
+            return;
+        }
+    };
+    if direction.0 {
+        result += "+ note: ";
+    } else {
+        result += "- note: ";
+    }
+    result += &state.3;
+    text.set_label(&result);
+}
+
+fn left_but_click(my_button: &Button) {
+    //Lock File Loop
+    let mut counter = 0;
+    while counter < 10 {
+        counter += 1;
+        let file = fs::OpenOptions::new().write(true).create_new(true).open("data.lock");
+        match file {
+            Ok(_) => break,
+            Err(error) => match error.kind() {
+                io::ErrorKind::AlreadyExists => println!("Game Locked"),
+                _ => panic!("Problem with lock file error {:?}", error),
+            },
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+    if counter > 9 {
+        return;
+    }
+    if mazegen::turn_cell(-1) {
+        // Update Labels
+        let first_grid = my_button.parent().unwrap()
+                                    .parent().unwrap()
+                                    .parent().unwrap();
+        let mut child_widget = first_grid.first_child().unwrap();
+        let state_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+        update_state_label(&state_label);
+        let label_grid = first_grid.last_child().unwrap();
+        child_widget = label_grid.first_child().unwrap()
+                                    .next_sibling().unwrap();
+        let mut dimension_counter = 0;
+        while dimension_counter < 4 {
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((true, dimension_counter), &child_label);
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((false, dimension_counter), &child_label);
+            dimension_counter += 1;
+        }
+        //println!("Turned Left");
+    } else {
+        println!("Error turning");
+    }
+    fs::remove_file("data.lock").unwrap();
+}
+
+fn right_but_click(my_button: &Button) {
+    //Lock File Loop
+    let mut counter = 0;
+    while counter < 10 {
+        counter += 1;
+        let file = fs::OpenOptions::new().write(true).create_new(true).open("data.lock");
+        match file {
+            Ok(_) => break,
+            Err(error) => match error.kind() {
+                io::ErrorKind::AlreadyExists => println!("Game Locked"),
+                _ => panic!("Problem with lock file error {:?}", error),
+            },
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+    if counter > 9 {
+        return;
+    }
+    if mazegen::turn_cell(1) {
+        // Update Labels
+        let first_grid = my_button.parent().unwrap()
+                                    .parent().unwrap()
+                                    .parent().unwrap();
+        let mut child_widget = first_grid.first_child().unwrap();
+        let state_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+        update_state_label(&state_label);
+        let label_grid = first_grid.last_child().unwrap();
+        child_widget = label_grid.first_child().unwrap()
+                                    .next_sibling().unwrap();
+        let mut dimension_counter = 0;
+        while dimension_counter < 4 {
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((true, dimension_counter), &child_label);
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((false, dimension_counter), &child_label);
+            dimension_counter += 1;
+        }
+        //println!("Turned Right");
+    } else {
+        println!("Error turning");
+    }
+    fs::remove_file("data.lock").unwrap();
+}
+
+fn back_but_click(my_button: &Button) {
+    //Lock File Loop
+    let mut counter = 0;
+    while counter < 10 {
+        counter += 1;
+        let file = fs::OpenOptions::new().write(true).create_new(true).open("data.lock");
+        match file {
+            Ok(_) => break,
+            Err(error) => match error.kind() {
+                io::ErrorKind::AlreadyExists => println!("Game Locked"),
+                _ => panic!("Problem with lock file error {:?}", error),
+            },
+        }
+        thread::sleep(Duration::from_secs(1));
+    }
+    if counter > 9 {
+        return;
+    }
+    if mazegen::turn_cell(0) {
+        // Update Labels
+        let first_grid = my_button.parent().unwrap()
+                                    .parent().unwrap()
+                                    .parent().unwrap();
+        let mut child_widget = first_grid.first_child().unwrap();
+        let state_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+        update_state_label(&state_label);
+        let label_grid = first_grid.last_child().unwrap();
+        child_widget = label_grid.first_child().unwrap()
+                                    .next_sibling().unwrap();
+        let mut dimension_counter = 0;
+        while dimension_counter < 4 {
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((true, dimension_counter), &child_label);
+            child_widget = child_widget.next_sibling().unwrap();
+            let child_label = child_widget.downcast_ref::<gtk::Label>().unwrap();
+            update_label((false, dimension_counter), &child_label);
+            dimension_counter += 1;
+        }
+        //println!("Turned Around");
+    } else {
+        println!("Error turning");
+    }
+    fs::remove_file("data.lock").unwrap();
+}
+
+fn make_note(input_text: &gtk::Entry) {
+    if !mazegen::store_note(input_text.buffer().text().clone()) {
+        println!("Error Saving Note");
+    } //else {
+        //println!("Saved");
+    //}
 }
 
 fn build_ui(app: &Application) {
@@ -91,6 +359,7 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .margin_bottom(12)
         .build();
+    left_but.connect_clicked(move |left_but| {left_but_click(left_but)});
     let right_but = Button::builder()
         .label("Next Direction")
         .margin_top(12)
@@ -98,6 +367,7 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .margin_bottom(12)
         .build();
+    right_but.connect_clicked(move |right_but| {right_but_click(right_but)});
     let back_but = Button::builder()
         .label("Rev Direction")
         .margin_top(12)
@@ -105,6 +375,9 @@ fn build_ui(app: &Application) {
         .margin_end(12)
         .margin_bottom(12)
         .build();
+    back_but.connect_clicked(move |back_but| {back_but_click(back_but)});
+    let text_input = gtk::Entry::new();
+    text_input.connect_activate(move |text_input| {make_note(text_input)});
 
     let button_grid = Grid::builder().build();
 
@@ -112,16 +385,17 @@ fn build_ui(app: &Application) {
     button_grid.attach(&left_but, 1, 2, 1, 1);
     button_grid.attach(&right_but, 2, 2, 1, 1);
     button_grid.attach(&back_but, 3, 2, 1, 1);
+    button_grid.attach(&text_input, 1, 3, 3, 1);
 
-    let text_ph = Label::new(Some("Text Here"));
+    let text_ph = Label::new(Some("Welcome to hyperspace\nNavigate using the buttons or arrow keys\nPress 'R' to restart\nMake notes with the input at the bottom"));
 
     let start_pane = Grid::builder().build();
     start_pane.set_row_homogeneous(true);
     start_pane.attach(&text_ph, 1, 1, 1, 1);
     start_pane.attach(&button_grid, 1, 2, 1, 1);
 
-    let text_space1 = Label::new(Some("|\n|\n|\n|\n|\n|\n|\n|"));
-    let text_space2 = Label::new(Some("|\n|\n|\n|\n|\n|\n|\n|"));
+    let text_space1 = Label::new(Some("|\n|\n|\n|\n|\n|\n|\n|\n|\n|"));
+    let text_space2 = Label::new(Some("|\n|\n|\n|\n|\n|\n|\n|\n|\n|"));
     let text_phx1 = Label::new(Some(".  x +  ."));
     let text_phx2 = Label::new(Some(".  x -  ."));
     let text_phy1 = Label::new(Some(".       ."));
@@ -132,6 +406,22 @@ fn build_ui(app: &Application) {
     text_pha1.set_margin_end(20);
     let text_pha2 = Label::new(Some(".       ."));
     text_pha2.set_margin_end(20);
+    update_label((true, 0), &text_phx1);
+    update_label((false, 0), &text_phx2);
+    update_label((true, 1), &text_phy1);
+    update_label((false, 1), &text_phy2);
+    update_label((true, 2), &text_phz1);
+    update_label((false, 2), &text_phz2);
+    update_label((true, 3), &text_pha1);
+    update_label((false, 3), &text_pha2);
+    //text_phx1.set_widget_name("x1");
+    //text_phx2.set_widget_name("x2");
+    //text_phy1.set_widget_name("y1");
+    //text_phy2.set_widget_name("y2");
+    //text_phz1.set_widget_name("z1");
+    //text_phz2.set_widget_name("z2");
+    //text_pha1.set_widget_name("a1");
+    //text_pha2.set_widget_name("a2");
 
     let end_pane = Grid::builder().build();
     end_pane.set_row_homogeneous(true);
@@ -146,10 +436,13 @@ fn build_ui(app: &Application) {
     end_pane.attach(&text_phz2, 4, 2, 1, 1);
     end_pane.attach(&text_pha1, 5, 1, 1, 1);
     end_pane.attach(&text_pha2, 5, 2, 1, 1);
+    end_pane.set_widget_name("end_pane");
 
     let state_label = Label::new(Some("Position: (0, 0, 0, 0) Facing: x +"));
+    update_state_label(&state_label);
     state_label.set_margin_top(10);
     state_label.set_margin_bottom(10);
+    state_label.set_widget_name("state");
     start_pane.set_margin_bottom(10);
     end_pane.set_margin_bottom(10);
 
@@ -163,5 +456,23 @@ fn build_ui(app: &Application) {
         .title("Hyperspace Navigation Training")
         .child(&outer_pane)
         .build();
+
+    let restart_act = gio::SimpleAction::new("restart", None);
+    let but_clone = forward_but.clone();
+    restart_act.connect_activate(move |_,_| {restart_game(&but_clone)});
+    window.add_action(&restart_act);
+    let forward_act = gio::SimpleAction::new("forward", None);
+    forward_act.connect_activate(move |_,_| {forward_but_click(&forward_but)});
+    window.add_action(&forward_act);
+    let left_act = gio::SimpleAction::new("turn_left", None);
+    left_act.connect_activate(move |_,_| {left_but_click(&left_but)});
+    window.add_action(&left_act);
+    let right_act = gio::SimpleAction::new("turn_right", None);
+    right_act.connect_activate(move |_,_| {right_but_click(&right_but)});
+    window.add_action(&right_act);
+    let back_act = gio::SimpleAction::new("turn_back", None);
+    back_act.connect_activate(move |_,_| {back_but_click(&back_but)});
+    window.add_action(&back_act);
+
     window.present();
 }
--- src/mazegen.rs
+++ src/mazegen.rs
@@ -13,6 +13,7 @@ pub struct GameState {
     position: u32,
     direction: (bool, u32),
     walls: Vec<bool>,
+    notes: Vec<String>,
 }
 
 pub fn make_maze(dimensions: u32) -> bool {
@@ -23,6 +24,7 @@ pub fn make_maze(dimensions: u32) -> bool {
         position: 0,
         direction: (true, 0),
         walls: vec![false; (dimensions*7*(1 << (dimensions - 1) * 3)).try_into().unwrap()],
+        notes: vec![String::from(" "); 1 << dimensions * 3],
     };
     let mut rng = rand::thread_rng();
     while wall_choices.len() > 0 {
@@ -34,10 +36,30 @@ pub fn make_maze(dimensions: u32) -> bool {
             //Add a 1% chance of this being true.
             continue;
         }
-        cell_match(cell_pair, dimensions, &mut cells);
+        cells = cell_match(cells, dimensions, cell_pair);
         game_state.walls[cur_wall as usize] = true;
     }
+    //println!("Cells: {:?}", cells);
     return save_game(&game_state);
+}
+
+fn cell_match(mut cells: Vec<u32>, dimensions: u32, cell_pair: (u32, u32)) -> Vec<u32> {
+    let low = cells[cell_pair.0 as usize];
+    let high = cells[cell_pair.1 as usize];
+    let mut check_cells: Vec<u32> = vec![cell_pair.1];
+    while check_cells.len() > 0 {
+        let cur_cell = check_cells.pop().unwrap();
+        cells[cur_cell as usize] = low;
+        for i in 0..dimensions {
+            if cur_cell >= (1 << 3 * i) && cells[(cur_cell - (1 << 3 * i)) as usize] == high {
+                check_cells.push(cur_cell - (1 << 3 * i));
+            }
+            if cur_cell + (1 << 3 * i) < 1 << 3 * dimensions && cells[(cur_cell + (1 << 3 * i)) as usize] == high {
+                check_cells.push(cur_cell + (1 << 3 * i));
+            }
+        }
+    }
+    return cells;
 }
 
 fn cell_pair(wall_ind: u32, dimensions: u32) -> (u32, u32) {
@@ -58,25 +80,7 @@ fn cell_pair(wall_ind: u32, dimensions: u32) -> (u32, u32) {
     return result;
 }
 
-fn cell_match(cell_pair: (u32, u32), dimensions: u32, cells: &mut Vec<u32>) {
-    let mut check_cells = vec![cell_pair.1];
-    while check_cells.len() > 0 {
-        let cur_cell = check_cells.pop().unwrap();
-        if cells[cur_cell as usize] == cell_pair.1 {
-            for i in 0..dimensions {
-                if cur_cell % (8 << i * 3) > (1 << i * 3) - 1 {
-                    check_cells.push(cur_cell - i * 8);
-                }
-                if cur_cell % (8 << i * 3) < (8 << i * 3) - (1 << i * 3) {
-                    check_cells.push(cur_cell + i * 8);
-                }
-            }
-            cells[cur_cell as usize] = cell_pair.0;
-        }
-    }
-}
-
-pub fn check_move(game_state: &GameState) -> bool {
+fn check_move(game_state: &GameState) -> bool {
     if game_state.direction.0 {
         if game_state.position % (8 << game_state.direction.1 * 3) < (8 << game_state.direction.1 * 3) - (1 << game_state.direction.1 * 3) {
             let walls_dimension = 7 * (1 << (game_state.dimensions - 1) * 3);
@@ -91,24 +95,113 @@ pub fn check_move(game_state: &GameState) -> bool {
     false
 }
 
+pub fn hall_dist(direction: (bool, u32)) -> Vec<String> {
+    let mut game_state = load_game();
+    let mut result: Vec<String> = vec![];
+    let mut counter = 0;
+    if game_state.dimensions <= direction.1 {
+        return result;
+    }
+    game_state.direction = direction;
+    while check_move(&game_state) && counter < 8 {
+        if game_state.direction.0 {
+            game_state.position += 1 << game_state.direction.1 * 3;
+        } else {
+            game_state.position -= 1 << game_state.direction.1 * 3;
+        }
+        result.push(game_state.notes[game_state.position as usize].clone());
+        counter += 1;
+    }
+    return result;
+}
+
+pub fn store_note(note: String) -> bool {
+    let mut game_state = load_game();
+    game_state.notes[game_state.position as usize] = note.clone();
+    return save_game(&game_state);
+}
+
+/*pub fn old_hall_dist(direction: (bool, u32)) -> i32 {
+    let mut game_state = load_game();
+    if game_state.dimensions <= direction.1 {
+        return -1;
+    }
+    game_state.direction = direction;
+    let mut result = 0;
+    while check_move(&game_state) {
+        result += 1;
+        if game_state.direction.0 {
+            game_state.position += 1 << game_state.direction.1 * 3;
+        } else {
+            game_state.position -= 1 << game_state.direction.1 * 3;
+        }
+    }
+    return result;
+}*/
+
+pub fn turn_cell(opt: i32) -> bool {
+    let mut game_state = load_game();
+    if game_state.position == (1 << 12) - 1 {
+        return false;
+    }
+    match opt {
+        -1 => {
+            if game_state.direction.1 == 0 {
+                game_state.direction.1 = game_state.dimensions - 1;
+                game_state.direction.0 = !game_state.direction.0;
+            } else {
+                game_state.direction.1 -= 1;
+            }
+        },
+        0 => {
+            game_state.direction.0 = !game_state.direction.0;
+        },
+        1 => {
+            if game_state.direction.1 == game_state.dimensions - 1 {
+                game_state.direction.1 = 0;
+                game_state.direction.0 = !game_state.direction.0;
+            } else {
+                game_state.direction.1 += 1;
+            }
+        },
+        _ => {
+            println!("Turning Went Wrong");
+            return false;
+        },
+    }
+    return save_game(&game_state);
+}
+
 pub fn move_cell() -> bool {
     let mut game_state = load_game();
+    if game_state.position == (1 << 12) - 1 {
+        return false;
+    }
     if check_move(&game_state) {
         if game_state.direction.0 {
             game_state.position += 1 << game_state.direction.1 * 3;
         } else {
             game_state.position -= 1 << game_state.direction.1 * 3;
         }
+        //println!("Position: {}", game_state.position);
+        if game_state.position == (1 << game_state.dimensions * 3) - 1 && game_state.dimensions < 4 {
+            return make_maze(game_state.dimensions + 1);
+        }
         return save_game(&game_state);
     }
     return false;
 }
 
+pub fn get_loc() -> (u32, u32, (bool, u32), String) {
+    let game_state = load_game();
+    (game_state.position, game_state.dimensions, game_state.direction, game_state.notes[game_state.position as usize].clone())
+}
+
 fn save_game(game_state: &GameState) -> bool {
     let result = save_file("data", 0, game_state);
     match result {
-        Ok(T) => return true,
-        Err(E) => return false,
+        Ok(_) => return true,
+        Err(_) => return false,
     }
 }
 
